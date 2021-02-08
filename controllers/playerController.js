cbd60bcf156709bd9500f1d2c171357f362ba7cf
HomeAlone <luke.gallagher.cst@gmail.com>
2021-02-08 20:22:11 +1100

    comment out code error

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -9,4 +9,26 @@ module.exports = {
             .then((dbGame) => res.json(dbGame))
             .catch((err) => res.status(422).json(err));
     },
+    // getPlayersInTeam: function (req, res) {
+    //     db.Player
+    //         .find({ teamId: mongoose.Types.ObjectId(`${req.params.id}`) })
+    //         .then((dbPlayers) => {
+    //             // get the teamIds that this player is in
+    //             let userIds = dbPlayers.map(function (player) {
+    //                 return mongoose.Types.ObjectId(player.userId)
+    //             });
+    //             // get the users associated with the returned players
+    //             db.User.find({ _id: { '$in': userIds } })
+    //                 .then((dbUsers) => {
+    //                     let players = dbUsers.map(function (user) {
+    //                         return { firstName: user.firstName, lastName: user.lastName, }
+    //                     })
+    //                     console.log(dbGameVotes);
+    //                     res.json(dbGameVotes);
+    //                 })
+    //                 .catch((err) => res.status(422).json(err));
+    //         })
+    //         .catch((err) => res.status(422).json(err));
+    // },
+ 
 };
