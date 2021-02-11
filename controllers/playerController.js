fc72e47fc80fe4ebb1b61e8ea31ea5df52716603
HomeAlone <luke.gallagher.cst@gmail.com>
2021-02-11 23:17:15 +1100

    add function to get players in a team

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -13,26 +13,29 @@ module.exports = {
             .then((dbPlayer) => res.json(dbPlayer))
             .catch((err) => res.status(422).json(err));
     },
-    // getPlayersInTeam: function (req, res) {
-    //     db.Player
-    //         .find({ teamId: mongoose.Types.ObjectId(`${req.params.id}`) })
-    //         .then((dbPlayers) => {
-    //             // get the teamIds that this player is in
-    //             let userIds = dbPlayers.map(function (player) {
-    //                 return mongoose.Types.ObjectId(player.userId)
-    //             });
-    //             // get the users associated with the returned players
-    //             db.User.find({ _id: { '$in': userIds } })
-    //                 .then((dbUsers) => {
-    //                     let players = dbUsers.map(function (user) {
-    //                         return { firstName: user.firstName, lastName: user.lastName, }
-    //                     })
-    //                     console.log(dbGameVotes);
-    //                     res.json(dbGameVotes);
-    //                 })
-    //                 .catch((err) => res.status(422).json(err));
-    //         })
-    //         .catch((err) => res.status(422).json(err));
-    // },
+    getPlayersInTeam: function (req, res) {
+        console.log('getting players in hte team');
+
+        db.Player
+            .find({ teamId: mongoose.Types.ObjectId(`${req.params.id}`) })
+            .then((dbPlayers) => {
+                console.log('got players in hte team');
+                
+                // get the userIds for players in the team
+                let userIds = dbPlayers.map(function (player) {
+                    return mongoose.Types.ObjectId(player.userId);
+                    // return player.userId
+                });
+                console.log(userIds);
+                // get the users associated with the returned players
+                // db.Game.find({ teamId: { '$in': team_ids }, votingOpen: true })
+                db.User.find({ _id: { '$in': userIds } })
+                    .then((dbUsers) => {
+                       res.json(dbUsers);
+                    })
+                    .catch((err) => res.status(422).json(err));
+            })
+            .catch((err) => res.status(422).json(err));
+    },
  
 };
